4353384fb41ef42a806fe4eef6027a41314b5e90
MahantaTalakal <148555051+MahantaTalakal@users.noreply.github.com>
2025-08-11 21:26:56 +0530

    Create adder_HLS_bareMetal.cpp

---
+++ adder_HLS/adder_HLS_bareMetal.cpp
@@ -0,0 +1,30 @@
+#include "xparameters.h"
+#include "xil_io.h"
+#include <stdio.h>
+
+#define ADDER_BASE    0xa0000000
+
+#define ADDR_A_OFFSET    0x10
+#define ADDR_B_OFFSET    0x18
+#define ADDR_SUM_OFFSET  0x20
+
+int main()
+{
+    u32 a = 123, b = 456;
+    u32 sum;
+
+    // Write inputs
+    Xil_Out32(ADDER_BASE + ADDR_A_OFFSET, a);
+    Xil_Out32(ADDER_BASE + ADDR_B_OFFSET, b);
+
+    // Read result
+    sum = Xil_In32(ADDER_BASE + ADDR_SUM_OFFSET);
+
+    // Print to UART
+   
+    while (1){
+         printf("a = %u, b = %u, sum = %u\n", a, b, sum);
+
+    }
+    return 0;
+}
